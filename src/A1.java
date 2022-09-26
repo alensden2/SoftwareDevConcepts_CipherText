4146b21e51cfc04cf98b273a33f607490a46c980
alensden <alensden@gmail.com>
2022-09-26 03:16:59 -0300

    first

--- src/A1.java
+++ src/A1.java
@@ -1,20 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class A1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         String plainTextPath = "";
         String cipherTextPath = "";
         // Creation of object of type SubstitutionCipher()
         SubstitutionCipher substitutionCipher = new SubstitutionCipher();
 
-        //taking inputs for file paths
-        // /home/cynos/IdeaProjects/Assignment1-cipherToPlainText/src/cipherA.txt /home/cynos/IdeaProjects/Assignment1-cipherToPlainText/src/lang1_plain.txt
+        // taking inputs for file paths
+        // Paths to be passed using '//' ex-""
+        // //home//cynos//IdeaProjects//Assignment1-cipherToPlainText//src//cipherA.txt //home//cynos//IdeaProjects//Assignment1-cipherToPlainText//src//lang1_plain.txt
 
         plainTextPath = sc.nextLine();
         cipherTextPath = sc.nextLine();
 
-        System.out.println(substitutionCipher.testWorks(plainTextPath,cipherTextPath));
+        //testing rig ---- remove
+        String nullExcep = null;
+
+        substitutionCipher.originalLanguage("lang A", plainTextPath);
+        substitutionCipher.ciphertext(cipherTextPath);
 
     }
 
--- src/SubstitutionCipher.java
+++ src/SubstitutionCipher.java
@@ -1,64 +1,85 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SubstitutionCipher {
+    // variable declaration
+
+    HashMap<Character, Double> plainTextFrequencyTable = new HashMap<Character, Double>();
+
     // empty constructor
-    SubstitutionCipher(){
+    SubstitutionCipher() {
 
     }
 
     // params constructor
-    SubstitutionCipher(String name, Map<Character,Character> key){
+    SubstitutionCipher(String name, Map<Character, Character> key) {
 
     }
 
     // gives freq map for plainText
     // name and filepath
-    boolean originalLanguage(String name, String fileName){
+    boolean originalLanguage(String name, String fileName) throws IOException {
+        if(name != null && fileName !=null){
+            if(name != "" && fileName !="" ){
+                Helper helper = new Helper();
+                plainTextFrequencyTable = helper.frequencyTable(fileName);
+                System.out.println(plainTextFrequencyTable);
+            }
+            else {
+                // if name or fileName is empty string
+                return false;
+            }
+        } else {
+            // if name or File path has null then throw false
+            return false;
+        }
         return true;
     }
 
     // gives freq map for cipherText
     // filepath
-    boolean ciphertext(String filename){
+    boolean ciphertext(String filename) {
         return true;
     }
 
     //decodes with the associated key
-    String decodeText(){
+    String decodeText() {
         String decodedText = "";
         return decodedText;
     }
 
     // update internal encryption, put to the map/replace
-    boolean setDecodeLetter(char plaintextChar, char ciphertextChar){
+    boolean setDecodeLetter(char plaintextChar, char ciphertextChar) {
         return true;
     }
 
     //get the current associated key
-    Map<Character,Character> getKey(){
-        Map<Character,Character> currentKey = new HashMap<Character,Character>();
+    Map<Character, Character> getKey() {
+        Map<Character, Character> currentKey = new HashMap<Character, Character>();
         return currentKey;
     }
 
     // if each freq matches with the plain text return true
-    boolean KeyIsValid(){
+    boolean KeyIsValid() {
         return true;
     }
 
     // makes custom key from matching the freq tables
     // name of the language
-    boolean guessKeyFromFrequencies(String language){
+    boolean guessKeyFromFrequencies(String language) {
         return true;
     }
 
-    String matchLanguage(){
+    String matchLanguage() {
         String langNameCoded = "";
         return langNameCoded;
     }
 
-    String testWorks(String a, String b){
-        return "paths are as follows" + a + " " + b;
+    String testWorks(String a, String b) throws IOException {
+
+        return "plainTextFrequencyTable works";
     }
 
 }
